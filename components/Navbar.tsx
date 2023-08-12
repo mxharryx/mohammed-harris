05a52bc81461fa23763b8332d369ff4bf14b11da
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-12 17:43:16 -0400

    Navbar fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,34 @@
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.scss'; // Import your styles
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <motion.div className={styles.logo} initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:1}}>Harris();</motion.div>
-            <div>
+            <motion.div className={styles.logo} initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }}>Harris();</motion.div>
             <ul className={styles.navLinks}>
-                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.1}}>
+                <motion.li initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.1 }}>
                     <Link href="/">//home</Link>
                 </motion.li>
-                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.2}}>
+                <motion.li initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.2 }}>
                     <Link href="/about">//about</Link>
                 </motion.li>
-                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.3}}>
+                <motion.li initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.3 }}>
                     <Link href="/Skills">//skills</Link>
                 </motion.li>
-                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.4}}>
+                <motion.li initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.4 }}>
                     <Link href="/Experience">//experience</Link>
                 </motion.li>
-                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.5}}>
+                <motion.li initial={{ y: -10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.1, delay: 0.5 }}>
                     <Link href="/Contact">//coffee?</Link>
-                </motion.li> 
+                </motion.li>
                 <a href="/assets/Resume-Harris.pdf" target="_blank">
-                    <motion.button className={styles.resumeButton} initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.5}} >Resume</motion.button>
+                    <motion.button className={styles.resumeButton} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }} >Resume</motion.button>
                 </a>
             </ul>
-            
-            </div>
-        
+
+
+
         </nav>
     );
 };
