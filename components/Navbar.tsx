0b087cbfbdd1863cd84fb8b0330db476d7e86aed
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-11 20:12:20 -0400

    Added Resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,38 @@
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.scss'; // Import your styles
+import {motion} from 'framer-motion';
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <div className={styles.logo}>Harris();</div>
+            <motion.div className={styles.logo} initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:1}}>Harris();</motion.div>
+            <div>
             <ul className={styles.navLinks}>
-                <li>
+                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.1}}>
                     <Link href="/">//home</Link>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.2}}>
                     <Link href="/about">//about</Link>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.3}}>
                     <Link href="/Skills">//skills</Link>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.4}}>
                     <Link href="/Experience">//experience</Link>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li initial={{y:-10, opacity:0}} animate={{y:0, opacity:1}} transition={{duration:0.1, delay:0.5}}>
                     <Link href="/Contact">//coffee?</Link>
-                </li>
+                </motion.li> 
+                <a href="/assets/Resume-Harris.pdf" target="_blank">
+                    <motion.button className={styles.resumeButton} initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.5}} >Resume</motion.button>
+                </a>
             </ul>
+            
+            </div>
+        
         </nav>
     );
 };
 
 export default Navbar;
+
