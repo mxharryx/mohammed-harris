8b35ecbc5d5df114d4e232aba19acb6ad4651ff8
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-13 20:41:14 -0400

    Icons Animated

--- components/Leftside.tsx
+++ components/Leftside.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import styles from "../styles/Leftside.module.scss";
 import { FaTwitter, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import{motion} from "framer-motion";
 
 const LeftSide =()=>{
     return (
     <div className={styles.iconStyles}>
-        <div className={styles.icon}>
+        <motion.div initial={{ x: -50, opacity: 0 }} // Initial position and opacity
+                animate={{ x: 0, opacity: 1 }} // Final position and opacity
+                transition={{ delay: 0.5, duration: 0.5 }} // Animation delay and duration
+                className={styles.icon}>
             <div className={styles.socialIcons}>
-                <a href="https://twitter.com" target="_blank"><FaTwitter/></a>
-                <a href="https://linkedin.com/" target="_blank"><FaLinkedin/></a>
-                <a href="https://instagram.com/" target="_blank"><FaInstagram/></a>
-                <a href="https://github.com/" target="_blank"><FaGithub/></a>
+                <a href="https://twitter.com/mohammedharris_" target="_blank"><FaTwitter/></a>
+                <a href="https://www.linkedin.com/in/mohammedharrisjinnah/" target="_blank"><FaLinkedin/></a>
+                <a href="https://www.instagram.com/_harryyyy_____/" target="_blank"><FaInstagram/></a>
+                <a href="https://github.com/mxharryx/" target="_blank"><FaGithub/></a>
+                <div className={styles.border}></div>
             </div>
-        </div>
+            
+        </motion.div>
         </div>
     );
 };
