96d28370c591bbe1b35c8530c70d98d9492f5078
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-14 12:30:56 -0400

    LeftSide bar

---
+++ portfolio/components/LeftSide.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FaGithub, FaLinkedin, FaInstagram, FaTwitter}from 'react-icons/fa';
+
+const LeftSide = () => {
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-end gap-4 text-textDarkGold'>
+        <div className='flex flex-col gap-4'>
+            <a href='https://github.com/mxharryx' target='_blank'>
+                <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center justify-center hover:text-textLightGold cursor-pointer hover:-translate-y-2 transition-all duration-300' >
+                    <FaGithub />
+                    </span>
+                </a>
+                <a href='https://linkedin.com/in/mohammedharrisjinnah' target='_blank'>
+                <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center justify-center hover:text-textLightGold cursor-pointer hover:-translate-y-2 transition-all duration-300' >
+                    <FaLinkedin />
+                    </span>
+                </a>
+                <a href='https://www.instagram.com/_harryyyy_____/' target='_blank'>
+                <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center justify-center hover:text-textLightGold cursor-pointer hover:-translate-y-2 transition-all duration-300' >
+                    <FaInstagram />
+                    </span>
+                </a>
+                <a href='https://twitter.com/mohammedharris_' target='_blank'>
+                <span className='w-10 h-10 text-xl bg-bodyColor rounded-full inline-flex items-center justify-center hover:text-textLightGold cursor-pointer hover:-translate-y-2 transition-all duration-300' >
+                    <FaTwitter />
+                    </span>
+                </a> 
+        </div>
+        <div className='w-[2px] h-32 bg-textDarkGold'></div>
+    </div>
+  )
+}
+
+export default LeftSide;
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,3 +1,4 @@
+import LeftSide from '@/components/LeftSide';
 import Navbar from '@/components/Navbar';
 import Head from 'next/head';
 
@@ -12,6 +13,17 @@ export default function Home() {
     </Head>
     <main className='w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overscroll-y-scroll'>
       <Navbar />
+      <div>
+        <div className='w-full h-[88vh] xl:flex items-center gap-20 justify-between'>
+          <div className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
+            <LeftSide />
+          </div>
+          <div>Middle</div>
+          <div>
+            <RightSide />
+          </div>
+        </div>
+      </div>
     </main>
     </>
   )
