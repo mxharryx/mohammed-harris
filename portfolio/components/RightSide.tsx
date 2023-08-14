c075bea85fb8306d820eb3a482dd98a00446da0e
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-14 14:20:25 -0400

    Banner Component

--- portfolio/components/RightSide.tsx
+++ portfolio/components/RightSide.tsx
@@ -5,7 +5,7 @@ import {motion }from "framer-motion";
 const RightSide = () => {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end gap-6 text-textDarkGold'>
-        <a href='mailto:mohammedharrisjinnah@gmail.com' className='text-sm w-42 tracking-wide text-textDarkGold'>
+        <a href='mailto:mohammedharrisjinnah@gmail.com' className='text-md w-42 tracking-wide text-textDarkGold  hover:text-textLightGold hover:-translate-y-2 transition-all duration-300'>
             <FaEnvelope />
         </a>
         <span className='w-[2px] h-52 bg-textDarkGold inline-flex'></span>
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar';
 import RightSide from '@/components/RightSide';
 import Head from 'next/head';
 import {motion} from 'framer-motion';
+import Banner from '@/components/Banner';
 
 export default function Home() {
   return (
@@ -20,17 +21,17 @@ export default function Home() {
           <motion.div 
           initial={{y: 50, opacity: 0}}
           animate={{y: 0, opacity: 1}}
-          transition={{delay: 0.5, duration: 0.5 }}
+          transition={{delay: 1.5, duration: 0.5 }}
           className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
             <LeftSide />
           </motion.div>
-          <div className='h-[88vh] mx-auto p-4'>
-            Middle
+          <div className='h-[88vh] w-full mx-auto p-4'>
+            <Banner />
           </div>
           <motion.div 
           initial={{y: 50, opacity: 0}}
           animate={{y: 0, opacity: 1}}
-          transition={{delay: 0.5, duration: 0.5 }}
+          transition={{delay: 1.5, duration: 0.5 }}
           className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'>
             <RightSide />
           </motion.div>
