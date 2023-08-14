39b523287693f93d02643c4d29125a51488ce2ee
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-13 20:20:45 -0400

    Social Icons

--- components/Leftside.tsx
+++ components/Leftside.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "../styles/Leftside.module.scss";
+import { FaTwitter, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 
 const LeftSide =()=>{
-    return <div>LeftSide</div>;
+    return (
+    <div className={styles.iconStyles}>
+        <div className={styles.icon}>
+            <div className={styles.socialIcons}>
+                <a href="https://twitter.com" target="_blank"><FaTwitter/></a>
+                <a href="https://linkedin.com/" target="_blank"><FaLinkedin/></a>
+                <a href="https://instagram.com/" target="_blank"><FaInstagram/></a>
+                <a href="https://github.com/" target="_blank"><FaGithub/></a>
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default LeftSide;
