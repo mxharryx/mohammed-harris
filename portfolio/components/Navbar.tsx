82beb506ba24945b2f009f5cd5196c44ed208182
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-14 12:00:07 -0400

    Resume Button

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -15,44 +15,51 @@ const Navbar = () => {
                 >
                     MOHAMMED-HARRIS();
                 </motion.div>
-                <div>
+                <div className='hidden mdl:inline-flex items-center gap-7'>
                     <ul className='flex text-[13px] gap-7'>
                         <Link href="#home" className='flex items-center gap-1 font-medium text-textDark hover:text-textLightGold cursor-pointer duration-300 nav-link'>
                         <motion.li 
                         initial={{y:-10,opacity:0}}
                         animate={{y:0, opacity:1}}
                         transition={{duration:0.1}}>
-                            <span>//</span>Home</motion.li>
+                            <span className='text-textLightGold'>//</span>Home</motion.li>
                         </Link>
                         <Link href="#about" className='flex items-center gap-1 font-medium text-textDark hover:text-textLightGold cursor-pointer duration-300 nav-link'>
                         <motion.li
                         initial={{y:-10,opacity:0}}
                         animate={{y:0, opacity:1}}
                         transition={{duration:0.2}}>
-                            <span>//</span>About</motion.li>
+                            <span className='text-textLightGold'>//</span>About</motion.li>
                         </Link>
                         <Link href="#skills" className='flex items-center gap-1 font-medium text-textDark hover:text-textLightGold cursor-pointer duration-300 nav-link'>
                         <motion.li
                         initial={{y:-10,opacity:0}}
                         animate={{y:0, opacity:1}}
                         transition={{duration:0.3}}>
-                            <span>//</span>Skills</motion.li>
+                            <span className='text-textLightGold'>//</span>Skills</motion.li>
                         </Link>
                         <Link href="#experience" className='flex items-center gap-1 font-medium text-textDark hover:text-textLightGold cursor-pointer duration-300 nav-link'>
                         <motion.li
                         initial={{y:-10,opacity:0}}
                         animate={{y:0, opacity:1}}
                         transition={{duration:0.4}}>
-                            <span>//</span>Experience</motion.li>
+                            <span className='text-textLightGold'>//</span>Experience</motion.li>
                         </Link>
                         <Link href="#coffee" className='flex items-center gap-1 font-medium text-textDark hover:text-textLightGold cursor-pointer duration-300 nav-link'>
                         <motion.li
                         initial={{y:-10,opacity:0}}
                         animate={{y:0, opacity:1}}
                         transition={{duration:0.5}}>
-                            <span>//</span>Coffee</motion.li>
+                            <span className='text-textLightGold'>//</span>Coffee</motion.li>
                         </Link>
                     </ul>
+                    <a href='/assets/Resume-Harris.pdf'>
+                    <motion.button
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
+                    transition={{delay:0.5}}
+                    className="px-4 py-2 rounded-md text-textDarkGold text-[13px] border border-textDarkGold hover:bg-hoverColor duration-300">Resume</motion.button>
+                    </a>
                 </div>
             </div>
         </div>
