3f2ca6b1a5a30ea4948634ae3adbc888ae6b3b67
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-08-14 11:05:24 -0400

    Document update

--- portfolio/pages/_document.tsx
+++ portfolio/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+      <Head >
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,300&family=Encode+Sans+Semi+Condensed&family=La+Belle+Aurore&family=Roboto&display=swap" rel="stylesheet"/>
+            </Head>
+            <body>
+              <Main />
+              <NextScript />
+            </body>
+          </Html>
+          )
 }
