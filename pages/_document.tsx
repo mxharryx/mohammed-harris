79af5e4c68ebcf7d21a152d6174d56ac91728c98
Mohammed Harris <md.harry.98@gmail.com>
2023-08-16 13:33:44 -0400

    Adding meta tags for sharing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
+        <link rel="icon" href="favicon.ico" sizes="32x32" type="image/ico" />
+        <link rel="icon" href="favicon.ico" sizes="16x16" type="image/ico" /> 
+        <link rel="apple-touch-icon" href="favicon.ico" type="image/ico"/>
+        <link rel="alternate" href="https://mohammed-harris.vercel.app" hrefLang="x-default" />
+        <link rel="canonical" href="https://mohammed-harris-vercel.app" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,300&family=Encode+Sans+Semi+Condensed&family=La+Belle+Aurore&family=Roboto&display=swap" rel="stylesheet"/>
-            </Head>
-            <body>
-              <Main />
-              <NextScript />
-            </body>
-          </Html>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,300&family=Encode+Sans+Semi+Condensed&family=La+Belle+Aurore&family=Roboto&display=swap" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
           )
 }
