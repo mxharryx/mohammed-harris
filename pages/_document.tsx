0b1c6c918ba2dd3c4189a093bd7bf60f78739296
Mohammed Harris <md.harry.98@gmail.com>
2023-08-16 13:55:25 -0400

    favicon dimensions

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <link rel="icon" href="favicon.ico" sizes="32x32" type="image/ico" />
-        <link rel="icon" href="favicon.ico" sizes="16x16" type="image/ico" /> 
+        <link rel="icon" href="favicon-32x32.ico" sizes="32x32" type="image/ico" />
+        <link rel="icon" href="favicon-16x16.ico" sizes="16x16" type="image/ico" /> 
         <link rel="apple-touch-icon" href="favicon.ico" type="image/ico"/>
         <link rel="alternate" href="https://mohammed-harris.vercel.app" hrefLang="x-default" />
         <link rel="canonical" href="https://mohammed-harris-vercel.app" />
